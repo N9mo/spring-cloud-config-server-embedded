a6bc8e6c5e3f2005ae5c9879dec4a6d395d675f8
Yury Zuzansky <yury.zuzansky@scalors.com>
2020-05-09 11:16:32 +0300

    Initial commit

---
+++ src/main/java/springcloudconfigserverembedded/SpringCloudConfigServerEmbeddedApplication.java
@@ -0,0 +1,13 @@
+package springcloudconfigserverembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerEmbeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerEmbeddedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springcloudconfigserverembedded/SpringCloudConfigServerEmbeddedApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloudconfigserverembedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerEmbeddedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
